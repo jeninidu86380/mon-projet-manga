adfa5fe042333ee9627a7362badc7819ee8b2de9
jennifer rozat <jenniferrozat@gmail.com>
2024-03-18 13:55:50 +0100

    modif

--- mon projet manga/index.php
+++ mon projet manga/index.php
@@ -1,5 +1,16 @@
-<?php 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projet manga</title>
+</head>
+<body>
+    <h1>Mon site de manga</h1>
 
-
-
-?>
+    <?php
+   
+    ?>
+    
+</body>
+</html>
